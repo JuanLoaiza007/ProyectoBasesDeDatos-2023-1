e8fad782d511cb023a551aa8845ab446605c0d6b
JuanDavid <juan.loaiza.santiago@correounivalle.edu.co>
2023-05-30 09:22:59 -0500

    Creacion de DAO inicial			(LibroAutor, Libro, Multa, Prestamo, PrestamoEjemplar)

---
+++ src/DAO/postgres/LibroDAOImpl.java
@@ -0,0 +1,211 @@
+package DAO.postgres;
+
+import Objetos.Libro;
+import Objetos.Libro;
+import Objetos.Libro;
+import Paneles.AvisosEmergentes;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Bases de datos 750006C-01
+ * Proyecto de curso
+ * Profesor: Oswaldo Solarte
+ * 
+ * Archivo: LibroDAOImpl.java
+ * Licencia: GNU-GPL
+ * @version 1.0
+ * 
+ * @author Alejandro Guerrero Cano      (202179652-3743) {@literal <"alejandro.cano@correounivalle.edu.co">} 
+ * @author Juan David Loaiza Santiago   (202177570-3743) {@literal <"juan.loaiza.santiago@correounivalle.edu.co">} 
+ * @author Juan Sebastian Muñoz Rojas   (202177436-3743) {@literal <"juan.munoz.rojas@correounivalle.edu.co">} 
+ * 
+ */
+
+public class LibroDAOImpl{
+    
+    private Connection conexion;
+
+    public LibroDAOImpl(Connection conexion) {
+        this.conexion = conexion;
+    }
+    
+    private Libro convertir(ResultSet result) throws SQLException{
+        Libro libro = null;
+
+        String isbn = result.getString("isbn");
+        String codigoArea = result.getString("codigo_area");
+        String codigoEditorial = result.getString("codigo_editorial");
+        String idEmpleado = result.getString("id_empleado");
+        String titulo = result.getString("titulo");
+        String anioPublicacion = result.getString("anio_publicacion");
+        int nroPaginas = result.getInt("nro_paginas");
+        
+        libro = new Libro(isbn, codigoArea, codigoEditorial, idEmpleado, titulo, anioPublicacion, nroPaginas);
+        
+        return libro;
+    }
+    
+    /**
+     * Cierra la conexion que se le pase como parametro
+     * @param conexion (Connection) La conexion a cerrar
+     */
+    private void cerrarConexion(Connection conexion) {
+        if (conexion != null) {
+            try {
+                conexion.close();
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+    
+    /**
+     * Cierra el statement que se le pase como parametro
+     * @param statement (PreparedStatement) el Statement a cerrar
+     */
+    private void cerrarStatement(PreparedStatement statement) {
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+
+    public void insertar(Libro e) {
+        //        libro (isbn, codigo_area, codigo_editorial, id_empleado, titulo, anio_publicacion, nro_paginas)
+        String INSERT = "INSERT INTO libro (isbn, codigo_area, codigo_editorial, id_empleado, titulo, anio_publicacion, nro_paginas) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement statement = null;
+        ResultSet result = null;
+
+        try {
+            statement = conexion.prepareStatement(INSERT);
+            statement.setString(1, e.getIsbn());
+            statement.setString(2, e.getCodigoArea());
+            statement.setString(3, e.getCodigoEditorial());
+            statement.setString(4, e.getIdEmpleado());
+            statement.setString(5, e.getTitulo());
+            statement.setString(6, e.getAnioPublicacion());
+            statement.setString(7, Integer.toString(e.getNroPaginas()));
+
+            if (statement.executeUpdate() == 0) {
+                System.out.println("Es posible que no se haya guardado la insercion");
+            }
+
+        } catch (SQLException ex) {
+            AvisosEmergentes.mostrarMensaje("" + ex.getErrorCode());
+        } finally {
+            cerrarConexion(conexion);
+            cerrarStatement(statement);
+        }
+    }
+
+    public void modificar(Libro e) {
+        String UPDATE = "UPDATE libro SET codigo_area = ?, codigo_editorial = ?, id_empleado = ?, titulo = ?, anio_publicacion = ?, nro_paginas = ? WHERE isbn = ?";
+
+        PreparedStatement statement = null;
+
+        try {
+            statement = conexion.prepareStatement(UPDATE);       
+            statement.setString(1, e.getCodigoArea());
+            statement.setString(2, e.getCodigoEditorial());
+            statement.setString(3, e.getIdEmpleado());
+            statement.setString(4, e.getTitulo());
+            statement.setString(5, e.getAnioPublicacion());
+            statement.setString(6, Integer.toString(e.getNroPaginas()));
+            statement.setString(7, e.getIsbn());
+
+            if (statement.executeUpdate() == 0) {
+                System.out.println("Es posible que no se haya modificado el registro");
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } finally {
+            cerrarConexion(conexion);
+            cerrarStatement(statement);
+        }
+    }
+
+    public void eliminar(Libro e) {
+        String DELETE = "DELETE FROM libro WHERE isbn = ?";
+
+        PreparedStatement statement = null;
+
+        try {
+            statement = conexion.prepareStatement(DELETE);
+            statement.setString(1, e.getIsbn());
+
+            if (statement.executeUpdate() == 0) {
+                System.out.println("Es posible que no se haya eliminado el registro");
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } finally {
+            cerrarConexion(conexion);
+            cerrarStatement(statement);
+        }
+    }
+
+    public List<Libro> obtenerTodos() {
+        List<Libro> libros = new ArrayList<>();
+
+        String GETALL = "SELECT isbn, codigo_area, codigo_editorial, id_empleado, titulo, anio_publicacion, nro_paginas FROM libro ORDER BY titulo ASC";
+
+        PreparedStatement statement = null;
+        ResultSet result = null;
+
+        try {
+
+            statement = conexion.prepareStatement(GETALL);
+            result = statement.executeQuery();
+
+            while (result.next()) {
+                libros.add(convertir(result));
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } finally {
+            cerrarConexion(conexion);
+            cerrarStatement(statement);
+        }
+
+        return libros;
+    }
+
+    public Libro obtener(String id) {
+        Libro libro = null;
+
+        String GETONE = "SELECT isbn, codigo_area, codigo_editorial, id_empleado, titulo, anio_publicacion, nro_paginas FROM libro WHERE isbn = ?";
+
+        PreparedStatement statement = null;
+        ResultSet result = null;
+
+        try {
+
+            statement = conexion.prepareStatement(GETONE);
+            statement.setString(1, id);
+            result = statement.executeQuery();
+
+            if (result.next()) {
+                libro = convertir(result);
+            } else {
+                System.out.println("No se ha encontrado un registro con ese Id");
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } finally {
+            cerrarConexion(conexion);
+            cerrarStatement(statement);
+        }
+        return libro;
+    }
+
+}
---
+++ src/DAO/postgres/PrestamoEjemplarDAOImpl.java
@@ -0,0 +1,190 @@
+package DAO.postgres;
+
+import Objetos.PrestamoEjemplar;
+import Paneles.AvisosEmergentes;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Bases de datos 750006C-01
+ * Proyecto de curso
+ * Profesor: Oswaldo Solarte
+ * 
+ * Archivo: PrestamoEjemplarDAOImpl.java
+ * Licencia: GNU-GPL
+ * @version 1.0
+ * 
+ * @author Alejandro Guerrero Cano      (202179652-3743) {@literal <"alejandro.cano@correounivalle.edu.co">} 
+ * @author Juan David Loaiza Santiago   (202177570-3743) {@literal <"juan.loaiza.santiago@correounivalle.edu.co">} 
+ * @author Juan Sebastian Muñoz Rojas   (202177436-3743) {@literal <"juan.munoz.rojas@correounivalle.edu.co">} 
+ * 
+ */
+
+public class PrestamoEjemplarDAOImpl {
+    
+    private Connection conexion;
+    private DateTimeFormatter dateFormato = DateTimeFormatter.ofPattern("yyyy/MM/d H:mm:ss"); 
+
+    public PrestamoEjemplarDAOImpl(Connection conexion) {
+        this.conexion = conexion;
+    }
+    
+    private PrestamoEjemplar convertir(ResultSet result) throws SQLException{
+        PrestamoEjemplar prestamoEjemplar = null;
+        
+        
+        String nroConsecutivoPrestamo = result.getString("nro_consecutivo_prestamo");
+        String isbn = result.getString("nro_consecutivo_prestamo");
+        String nroEjemplar = result.getString("nro_consecutivo_prestamo");
+        LocalDateTime fechaDevolucion = LocalDateTime.parse(result.getString("nro_consecutivo_prestamo"));
+       
+        
+        prestamoEjemplar = new PrestamoEjemplar(nroConsecutivoPrestamo, isbn, nroEjemplar, fechaDevolucion);
+        
+        return prestamoEjemplar;
+    }
+    
+    /**
+     * Cierra la conexion que se le pase como parametro
+     * @param conexion (Connection) La conexion a cerrar
+     */
+    private void cerrarConexion(Connection conexion) {
+        if (conexion != null) {
+            try {
+                conexion.close();
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+    
+    /**
+     * Cierra el statement que se le pase como parametro
+     * @param statement (PreparedStatement) el Statement a cerrar
+     */
+    private void cerrarStatement(PreparedStatement statement) {
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+
+    public void insertar(PrestamoEjemplar e) {
+        String INSERT = "INSERT INTO prestamo_ejemplar (nro_consecutivo_prestamo, isbn, nro_ejemplar, fecha_devolucion) VALUES (?, ?, ?, ?)";
+
+        PreparedStatement statement = null;
+        ResultSet result = null;
+
+        try {
+            statement = conexion.prepareStatement(INSERT);
+            statement.setString(1, e.getNroConsecutivoPrestamo());
+            statement.setString(2, e.getIsbn());
+            statement.setString(3, e.getNroEjemplar());
+            statement.setString(4, e.getFechaDevolucion().format(dateFormato));
+
+            if (statement.executeUpdate() == 0) {
+                System.out.println("Es posible que no se haya guardado la insercion");
+            }
+
+        } catch (SQLException ex) {
+            AvisosEmergentes.mostrarMensaje("" + ex.getErrorCode());
+        } finally {
+            cerrarConexion(conexion);
+            cerrarStatement(statement);
+        }
+    }
+
+    public void modificar(PrestamoEjemplar antiguo, PrestamoEjemplar nuevo) {
+        eliminar(antiguo);
+        insertar(nuevo);
+    }
+
+    public void eliminar(PrestamoEjemplar e) {
+        String DELETE = "DELETE FROM prestamo_ejemplar WHERE nro_consecutivo_prestamo = ? AND isbn = ? AND nro_ejemplar = ? AND fecha_devolucion = ?";
+
+        PreparedStatement statement = null;
+
+        try {
+            statement = conexion.prepareStatement(DELETE);
+            statement.setString(1, e.getNroConsecutivoPrestamo());
+            statement.setString(2, e.getIsbn());
+            statement.setString(3, e.getNroEjemplar());
+            statement.setString(4, e.getFechaDevolucion().format(dateFormato));
+
+            if (statement.executeUpdate() == 0) {
+                System.out.println("Es posible que no se haya eliminado el registro");
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } finally {
+            cerrarConexion(conexion);
+            cerrarStatement(statement);
+        }
+    }
+
+    public List<PrestamoEjemplar> obtenerTodos() {
+        List<PrestamoEjemplar> prestamosEjemplares = new ArrayList<>();
+
+        String GETALL = "SELECT nro_consecutivo_prestamo, isbn, nro_ejemplar, fecha_devolucion FROM prestamo ORDER BY fecha_devolucion ASC";
+
+        PreparedStatement statement = null;
+        ResultSet result = null;
+
+        try {
+
+            statement = conexion.prepareStatement(GETALL);
+            result = statement.executeQuery();
+
+            while (result.next()) {
+                prestamosEjemplares.add(convertir(result));
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } finally {
+            cerrarConexion(conexion);
+            cerrarStatement(statement);
+        }
+
+        return prestamosEjemplares;
+    }
+
+    /**
+     * Obtener por nroConsecutivoPrestamo
+     * @param id nroConsecutivoPrestamo
+     * @return Los prestamoEjemplar de ese nroConsecutivoPrestamo
+     */
+    public List<PrestamoEjemplar> obtener(String id) {
+        List<PrestamoEjemplar> prestamosEjemplares = new ArrayList<>();
+
+        String GETALL = "SELECT nro_consecutivo_prestamo, isbn, nro_ejemplar, fecha_devolucion FROM prestamo WHERE nro_consecutivo_prestamo = ? ORDER BY fecha_devolucion ASC";
+
+        PreparedStatement statement = null;
+        ResultSet result = null;
+
+        try {
+
+            statement = conexion.prepareStatement(GETALL);
+            result = statement.executeQuery();
+
+            while (result.next()) {
+                prestamosEjemplares.add(convertir(result));
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } finally {
+            cerrarConexion(conexion);
+            cerrarStatement(statement);
+        }
+
+        return prestamosEjemplares;
+    }
+}
